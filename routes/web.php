f4b562af212a5b1eed99864836116d35655e0331
Abdul-Qoyyum <nurudeen96qoyyum@gmail.com>
2020-08-04 15:42:24 -0400

    Initialise git

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::view('/', 'home.index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::view('features','home.features')->name('home.features');
+
+Route::view('pricing','home.pricing')->name('home.pricing');
+
+Route::view('about', 'home.about')->name('home.about');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
