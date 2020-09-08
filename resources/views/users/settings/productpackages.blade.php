cc2bcfd54b3cfde9a478e712c89b31e6a3335727
haroonahmed0044 <haroonahmed0044@gmail.com>
2020-09-09 01:14:53 +0500

    first commit

--- resources/views/users/settings/productpackages.blade.php
+++ resources/views/users/settings/productpackages.blade.php
@@ -4,21 +4,16 @@
 
 <div class="row user-section mt-4 ">
     <div class="col-sm-6">
-    <h4 _ngcontent-c8="" class="settings-section-header-title ml-2">Users</h4>
+    <h4 _ngcontent-c8="" class="settings-section-header-title ml-2">Company Setting</h4>
     <nav aria-label="breadcrumb ">
     <ol class="breadcrumb bg-white m-1 p-1">
       <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">Dashboard</a></li>
       <li class="breadcrumb-item"><a href="{{ url('settings') }}">Setting</a></li>
-      <li class="breadcrumb-item active" aria-current="page">Contact form</li>
+      <li class="breadcrumb-item active" aria-current="page">Product Packages</li>
     </ol>
   </nav>
 </div>
   <div class=" col-sm-6 text-sm-right">
-    <div class="clearfix  hidden visible-xs"></div>
-    <button type="button" class="btn btn-success btn-add-new" data-toggle="modal" data-target="#invite-company-user-modal">
-        Invite company user
-    </button>
-    {{-- Invite user model --}}
     <div class="modal fade bd-example-modal-lg" id="invite-company-user-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
           <div class="modal-content">
@@ -65,15 +60,24 @@
         <table class="table table-striped">
             <thead>
             <tr style="border-top: 1px solid #eee;">
-                <th>contact form</th>
-                <th>Email</th>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Quantity</th>
                 <th></th>
             </tr>
             </thead>
             <tbody>
             <tr ng-repeat="user in users | orderBy" class="ng-scope">
-                <td class="ng-binding">Tanveer</td>
-                <td class="ng-binding">zari@gmail.com</td>
+                <td class="ng-binding">ID</td>
+                <td class="ng-binding">Bronze Family Portrait Package</td>
+                <td class="ng-binding"><p>The perfect starter package for families with high-resolution images.</p><ul>
+                    <li>1 Hour photography session</li>
+                    <li>10 High resolution digital files, retouched and ready to print</li>
+                    </ul></td>
+                <td class="ng-binding">995</td>
+                <td class="ng-binding">1</td>
                 <td class="user-button text-right td-btn-group">
                     <button style="margin-bottom: 0; margin-top: -7px"  type="submit" class="btn btn-default " data-toggle="modal" data-target="#edit-modal"><i class="fas fa-edit"></i></button>
                     <button style="margin-bottom: 0; margin-top: -7px"  ng-click="showDeleteModal($index)" class="btn btn-delete btn btn-delete btn-delete-empty  "><i class="fas fa-trash-alt"></i></button>
